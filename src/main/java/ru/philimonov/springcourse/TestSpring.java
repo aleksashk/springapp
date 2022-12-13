e146d6497f8173c65c6d3e9e96f5dd6536695d3f
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2022-12-13 16:51:20 +0300

    T05 factory-method

--- src/main/java/ru/philimonov/springcourse/TestSpring.java
+++ src/main/java/ru/philimonov/springcourse/TestSpring.java
@@ -5,10 +5,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        ClassicalMusic classicalMusic1 = context.getBean("musicBean", ClassicalMusic.class);
-        ClassicalMusic classicalMusic2 = context.getBean("musicBean", ClassicalMusic.class);
-        System.out.println(classicalMusic1.getSong());
-        System.out.println(classicalMusic2.getSong());
+        ClassicalMusic classicalMusic = context.getBean("musicBean", ClassicalMusic.class);
+        System.out.println(classicalMusic.getSong());
 //        MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
 //        MusicPlayer musicPlayer2 = context.getBean("musicPlayer", MusicPlayer.class);
 //        musicPlayer1.playMusic();
