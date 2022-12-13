4df0c778c94927699a703f746d1eaaa546e1b952
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2022-12-13 16:17:48 +0300

    T03 DI with get/set and applicationContext.xml

--- src/main/java/ru/philimonov/springcourse/MusicPlayer.java
+++ src/main/java/ru/philimonov/springcourse/MusicPlayer.java
@@ -2,13 +2,39 @@ package ru.philimonov.springcourse;
 
 public class MusicPlayer {
     private Music music;
-
+    private String name;
+    private int volume;
+    public MusicPlayer() {
+    }
     //IoC
-    public MusicPlayer(Music music) {
+//    public MusicPlayer(Music music) {
+//        this.music = music;
+//    }
+    public void playMusic(){
+        System.out.println("Playing " + music.getSong());
+    }
+
+    public Music getMusic() {
+        return music;
+    }
+
+    public void setMusic(Music music) {
         this.music = music;
     }
 
-    public void playMusic(){
-        System.out.println("Playing " + music.getSong());
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
     }
 }
