bd1a8e18f14f0b3cf62101ca8bbbb2f524d2c580
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2022-12-13 17:37:35 +0300

    T06 annotation Component, Autowired in set method

--- src/main/java/ru/philimonov/springcourse/ClassicalMusic.java
+++ src/main/java/ru/philimonov/springcourse/ClassicalMusic.java
@@ -1,9 +1,6 @@
 package ru.philimonov.springcourse;
 
-import org.springframework.stereotype.Component;
-
-@Component
-public class ClassicalMusic implements Music{
+public class ClassicalMusic implements Music {
     @Override
     public String getSong() {
         return "Hungarian Rhapsody";
