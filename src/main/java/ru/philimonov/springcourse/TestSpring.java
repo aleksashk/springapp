1ff12b3b4c146c412eab5b3dc2ab5f42cb282792
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2022-12-13 16:33:27 +0300

    T04 default scope singleton

--- src/main/java/ru/philimonov/springcourse/TestSpring.java
+++ src/main/java/ru/philimonov/springcourse/TestSpring.java
@@ -6,10 +6,14 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
+        MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer musicPlayer2 = context.getBean("musicPlayer", MusicPlayer.class);
+        musicPlayer1.playMusic();
+        System.out.println(musicPlayer1.getName());
+        System.out.println(musicPlayer1.getVolume());
+        System.out.println("musicPlayer1 = musicPlayer2 -> " + (musicPlayer1 == musicPlayer2));
+        System.out.println("musicPlayer1 = " + musicPlayer1);
+        System.out.println("musicPlayer2 = " + musicPlayer2);
 
         context.close();
     }
