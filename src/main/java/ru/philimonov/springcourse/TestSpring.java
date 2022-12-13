ac6e5efc1418911bd8fa74db8d7c5d6f29b44322
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2022-12-13 16:47:15 +0300

    T05 init method, destroy method with scope prototype

--- src/main/java/ru/philimonov/springcourse/TestSpring.java
+++ src/main/java/ru/philimonov/springcourse/TestSpring.java
@@ -5,8 +5,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        ClassicalMusic classicalMusic = context.getBean("musicBean", ClassicalMusic.class);
-        System.out.println(classicalMusic.getSong());
+        ClassicalMusic classicalMusic1 = context.getBean("musicBean", ClassicalMusic.class);
+        ClassicalMusic classicalMusic2 = context.getBean("musicBean", ClassicalMusic.class);
+        System.out.println(classicalMusic1.getSong());
+        System.out.println(classicalMusic2.getSong());
 //        MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
 //        MusicPlayer musicPlayer2 = context.getBean("musicPlayer", MusicPlayer.class);
 //        musicPlayer1.playMusic();
