2b6547139d0bfa99bb27f56dda695e08e725a47d
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2022-12-13 16:45:10 +0300

    T05 init method, destroy method

--- src/main/java/ru/philimonov/springcourse/ClassicalMusic.java
+++ src/main/java/ru/philimonov/springcourse/ClassicalMusic.java
@@ -1,8 +1,19 @@
 package ru.philimonov.springcourse;
 
 public class ClassicalMusic implements Music{
+
+    public void doMyInit(){
+        System.out.println("Doing my initialization!");
+    }
+
+    public void doMyDestroy(){
+        System.out.println("Doing my destruction!");
+    }
+
     @Override
     public String getSong() {
         return "Hungarian Rhapsody";
     }
+
+
 }
--- src/main/java/ru/philimonov/springcourse/TestSpring.java
+++ src/main/java/ru/philimonov/springcourse/TestSpring.java
@@ -5,18 +5,19 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-
-        MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer musicPlayer2 = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer1.playMusic();
-        System.out.println("musicPlayer1.getVolume -> " + musicPlayer1.getVolume());
-        System.out.println("musicPlayer2.getVolume -> " + musicPlayer2.getVolume());
-        System.out.println("musicPlayer1 = musicPlayer2 -> " + (musicPlayer1 == musicPlayer2));
-        System.out.println("musicPlayer1 = " + musicPlayer1);
-        System.out.println("musicPlayer2 = " + musicPlayer2);
-        musicPlayer1.setVolume(400);
-        System.out.println("musicPlayer1.getVolume -> " + musicPlayer1.getVolume());
-        System.out.println("musicPlayer2.getVolume -> " + musicPlayer2.getVolume());
+        ClassicalMusic classicalMusic = context.getBean("musicBean", ClassicalMusic.class);
+        System.out.println(classicalMusic.getSong());
+//        MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer musicPlayer2 = context.getBean("musicPlayer", MusicPlayer.class);
+//        musicPlayer1.playMusic();
+//        System.out.println("musicPlayer1.getVolume -> " + musicPlayer1.getVolume());
+//        System.out.println("musicPlayer2.getVolume -> " + musicPlayer2.getVolume());
+//        System.out.println("musicPlayer1 = musicPlayer2 -> " + (musicPlayer1 == musicPlayer2));
+//        System.out.println("musicPlayer1 = " + musicPlayer1);
+//        System.out.println("musicPlayer2 = " + musicPlayer2);
+//        musicPlayer1.setVolume(400);
+//        System.out.println("musicPlayer1.getVolume -> " + musicPlayer1.getVolume());
+//        System.out.println("musicPlayer2.getVolume -> " + musicPlayer2.getVolume());
 
         context.close();
     }
